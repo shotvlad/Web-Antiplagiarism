ce364c525d666a6f98359cb8e148d5d90f532a52
ivan.mamai00@mail.ru <250219As>
2021-04-26 00:49:11 +0300

    done

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
 
         const request = new XMLHttpRequest()
-        request.open('POST', 'http://34.86.152.21:8080/cgi-bin/script.cgi', true)
+        request.open('POST', 'http://35.230.127.115:8080/cgi-bin/script.cgi', true)
 
         request.addEventListener('readystatechange', function () {
             console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
